e4294ba61488311c736b0e6a72b5c2b4559d41f5
Aksana Bandarenka <oksanakyxnove@icloud.com>
2024-08-16 15:35:37 +0200

    feat: add mobile-menu and click

---
+++ shelter/main.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger-menu');
+const pop_up_menu = document.querySelector('.pop-up-menu');
+const menu = document.querySelector('.mobile-menu');
+burger.addEventListener('click', () => {
+    document.body.classList.toggle('open');
+});

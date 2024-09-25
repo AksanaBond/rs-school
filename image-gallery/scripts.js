0e502307713e41423de5b470993bad346b077b3d
Aksana Bandarenka <oksanakyxnove@icloud.com>
2024-09-25 23:07:01 +0200

    feat: display all imgs from Unplash  API and display new imgs when put search world

--- image-gallery/scripts.js
+++ image-gallery/scripts.js
@@ -1,14 +1,35 @@
-const url = "https://api.unsplash.com/search/photos?query=spring&client_id=gl2ROvG6eEHQ_sRkXSHi4fE7i3rxrNfSwLAavbVm60E";
-const img = document.querySelector(".img");
+let url =
+  "https://api.unsplash.com/search/photos?query=spring&client_id=gl2ROvG6eEHQ_sRkXSHi4fE7i3rxrNfSwLAavbVm60E";
+const button = document.getElementById("button");
+const mySearch = document.getElementById("mySearch");
+
+const img_container = document.getElementById("img-container");
 
 async function getData() {
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-    showData(data);
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+  while (img_container.childElementCount > 1) {
+    img_container.removeChild(img_container.childNodes[0]);
+  }
+  const img = document.querySelector(".img");
+  for (let i = 0; i < data.results.length - 1; i++) {
+    img_container.innerHTML += img.outerHTML;
+    console.log(img_container);
+  }
+  for (let i = 0; i <= data.results.length - 1; i++) {
+    showData(img_container.children[i], data.results[i]);
+  }
 }
 getData();
-function showData(data){
-    const imageUrl = data.results[0].urls.regular;
-    return img.src = `${imageUrl}`
+
+function showData(element, dataResult) {
+  const imageUrl = dataResult.urls.regular;
+  return (element.src = `${imageUrl}`);
 }
+
+button.addEventListener("click", () => {
+  const querysearch = mySearch.value;
+  url = `https://api.unsplash.com/search/photos?query=${querysearch}&client_id=gl2ROvG6eEHQ_sRkXSHi4fE7i3rxrNfSwLAavbVm60E`;
+  getData();
+});

c40183ae9750b07d68327e019d1192b6ee381ac6
Aksana Bandarenka <oksanakyxnove@icloud.com>
2024-10-01 18:44:10 +0200

    feat: add header footer background and one card and style

---
+++ random-game/scripts.js
@@ -0,0 +1,51 @@
+const card = document.querySelector(".card");
+const cards = [
+    {
+        numer: 1,
+        src: "assets/monsters/xy30_r2fo_230926.jpg",
+    },
+    {
+        numer: 2,
+        src: "assets/monsters/7af2_57w2_230920.jpg",
+    },
+    {
+        numer: 3,
+        src: "assets/monsters/8kem_grj9_230920.jpg",
+    },
+    {
+        numer: 4,
+        src: "assets/monsters/8spp_yvjr_230920.jpg",
+    },
+    {
+        numer: 5,
+        src: "assets/monsters/2001.i203.007_cartoon_monster_set-02.jpg",
+    },
+    {
+        numer: 6,
+        src: "assets/monsters/8969806.png",
+    },
+    {
+        numer: 7,
+        src: "assets/monsters/a0eo_uwde_230920.jpg",
+    },
+    {
+        numer: 8,
+        src: "assets/monsters/dkdy_5fgc_230926.jpg",
+    },
+    {
+        numer: 9,
+        src: "assets/monsters/jsln_5dxv_230920.jpg",
+    },
+    {
+        numer: 10,
+        src: "assets/monsters/tdbh_la32_230920.jpg",
+    },
+    {
+        numer: 11,
+        src: "assets/monsters/v2zo_u0et_230926.jpg",
+    },
+    {
+        numer: 12,
+        src: "assets/monsters/xvp5_shog_230920.jpg",
+    },
+];

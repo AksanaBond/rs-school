aefd124538ca2e008e725cf9b78c1ef3a5670583
Aksana Bandarenka <oksanakyxnove@icloud.com>
2024-10-06 22:56:42 +0200

    feat:add button start and local storage, add modal endgame

--- random-game/scripts.js
+++ random-game/scripts.js
@@ -1,51 +1,51 @@
 const card = document.querySelector(".card");
 const cards = [
   {
-    numer: 1,
+    number: "1",
     src: "assets/monsters/xy30_r2fo_230926.jpg",
   },
   {
-    numer: 2,
+    number: "2",
     src: "assets/monsters/7af2_57w2_230920.jpg",
   },
   {
-    numer: 3,
+    number: "3",
     src: "assets/monsters/8kem_grj9_230920.jpg",
   },
   {
-    numer: 4,
+    number: "4",
     src: "assets/monsters/8spp_yvjr_230920.jpg",
   },
   {
-    numer: 5,
+    number: "5",
     src: "assets/monsters/2001.i203.007_cartoon_monster_set-02.jpg",
   },
   {
-    numer: 6,
+    number: "6",
     src: "assets/monsters/8969806.png",
   },
   {
-    numer: 7,
+    number: "7",
     src: "assets/monsters/a0eo_uwde_230920.jpg",
   },
   {
-    numer: 8,
+    number: "8",
     src: "assets/monsters/dkdy_5fgc_230926.jpg",
   },
   {
-    numer: 9,
+    number: "9",
     src: "assets/monsters/jsln_5dxv_230920.jpg",
   },
   {
-    numer: 10,
+    number: "10",
     src: "assets/monsters/tdbh_la32_230920.jpg",
   },
   {
-    numer: 11,
+    number: "11",
     src: "assets/monsters/v2zo_u0et_230926.jpg",
   },
   {
-    numer: 12,
+    number: "12",
     src: "assets/monsters/xvp5_shog_230920.jpg",
   },
 ];
@@ -69,17 +69,55 @@ function fillcards() {
     container.innerHTML += card.outerHTML;
     console.log(container);
   }
-}
-fillcards();
-const img = document.querySelectorAll(".monster");
-img.forEach((element, index) => {
+
+  const img = document.querySelectorAll(".monster");
+  img.forEach((element, index) => {
     element.src = cardsAll[index].src;
-});
+    element.dataset.number = cardsAll[index].number;
+  });
+
+  const cardElements = document.querySelectorAll(".card");
+  cardElements.forEach((element, index) => {
+    element.dataset.number = cardsAll[index].number;
+  });
+}
+
+fillcards();
+
 // переворачивание карты
-const cardsforFlip = document.querySelectorAll('.card')
- let flippedCard = false;
- let firstCard, secondCard;
-cardsforFlip.forEach(card => card.addEventListener('click', ()=>{
-    card.classList.toggle('flipped');
-}));
-// Переворачивание 
+const cardsforFlip = document.querySelectorAll(".card");
+let flippedCard = false;
+let firstCard, secondCard;
+function cardFlip() {
+  this.classList.add("flipped");
+  if (!flippedCard) {
+    flippedCard = true;
+    firstCard = this;
+    return;
+  } else if (this !== firstCard) {
+    flippedCard = false;
+    secondCard = this;
+    compareCard();
+  }
+}
+
+cardsforFlip.forEach((card) => card.addEventListener("click", cardFlip));
+// Compare card
+function compareCard() {
+  if (firstCard.dataset.number === secondCard.dataset.number) {
+    firstCard.removeEventListener("click", cardFlip);
+    secondCard.removeEventListener("click", cardFlip);
+    return;
+  } else {
+    resetcard();
+  }
+}
+// перевернуть карты если нет совпадения
+function resetcard() {
+  const cardToFlipBack1 = firstCard;
+  const cardToFlipBack2 = secondCard;
+  setTimeout(() => {
+    cardToFlipBack1.classList.remove("flipped");
+    cardToFlipBack2.classList.remove("flipped");
+  }, 1000);
+}
